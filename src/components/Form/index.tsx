87ccd419ba4ef3980e102722beac4444d59170b2
Brendo Vale <brendo.vale02@gmail.com>
2023-03-19 02:08:12 -0400

    Implement form action

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from './styles'
@@ -5,16 +6,29 @@ import { colors } from "../../theme/colors";
 
 import PlusSvg from '../../assets/plus.svg'
 
-export function Form() {
+type FormProps = {
+  onAdd: (title: string) => void
+}
+
+export function Form({ onAdd }: FormProps) {
+  const [title, setTitle] = useState<string>('')
+
+  function handleAdd(){
+    onAdd(title)
+    setTitle('')
+  }
+
   return (
     <View style={styles.form}>
       <TextInput
         style={styles.textInput}
         placeholder="Adicione uma nova tarefa"
-        placeholderTextColor={colors.gray[300]} />
-        <TouchableOpacity onPress={() => {}} style={styles.button}>
-          <PlusSvg width={16} height={16}/>
-        </TouchableOpacity>
+        placeholderTextColor={colors.gray[300]}
+        onChangeText={setTitle}
+        value={title}/>
+      <TouchableOpacity onPress={handleAdd} style={styles.button}>
+        <PlusSvg width={16} height={16} />
+      </TouchableOpacity>
     </View>
   )
 }
